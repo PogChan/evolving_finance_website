84a500b67ac0f1f82f17bdf0def208754e96c25f
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2024-11-09 23:02:43 -0500

    docker works

--- backend/efiBackend/settings.py
+++ backend/efiBackend/settings.py
@@ -1,7 +1,44 @@
 import os
 from pathlib import Path
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # You can specify template directories here if needed
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key")
 print("Database name:", os.getenv('POSTGRES_DB'))
+
 # Base directory of the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,13 +50,13 @@ DEBUG = ENVIRONMENT == 'development'
 
 # Set allowed hosts for development vs production
 ALLOWED_HOSTS = ['localhost'] if DEBUG else ['efiterminal.com']
-
+ROOT_URLCONF = 'efiBackend.urls'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'NAME': os.getenv('POSTGRES_DB', 'efiterminal'),
+        'USER': os.getenv('POSTGRES_USER', 'pogchan'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '111844czy'),
         'HOST': 'db',
         'PORT': '5432',
     }
